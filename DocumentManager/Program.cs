7d06200a915c84f061a3ff6184264cbe2582d0c5
Julian <jaguirre@ibiscreativa.co>
2020-04-11 13:39:41 -0500

    Document Start

---
+++ DocumentManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DocumentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
